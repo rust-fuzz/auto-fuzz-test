a2aab6bc01675e5bfd3db46f4e762849b323d190
Finkelman <finkelman@semcog.org>
2019-02-13 23:21:48 -0500

    combine printing code

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,46 @@
 use quote::quote;
-use syn::{visit::Visit, File, FnArg, ImplItem, ItemFn, ItemImpl};
+use syn::{visit::Visit, File, FnArg, FnDecl, Ident, ImplItem, ItemFn, ItemImpl};
+
+fn print_a_test(this: Option<&str>, ident: &Ident, decl: &FnDecl) {
+    print!("proptest! {{ #[test] fn test_{}_fuzz (", ident);
+    if let Some(self_type) = &this {
+        print!("self_like_thing: Any::<{}>(), ", self_type);
+    }
+    for a in &decl.inputs {
+        if let FnArg::Captured(a) = a {
+            let pat = &a.pat;
+            let ty = &a.ty;
+            print!("{}: Any::<{}>(), ", quote!(#pat), quote!(#ty));
+        }
+    }
+    print!(") {{");
+    if this.is_some() {
+        print!("self_like_thing.");
+    }
+    print!("{} (", ident);
+    for a in &decl.inputs {
+        if let FnArg::Captured(a) = a {
+            let pat = &a.pat;
+            print!("{}, ", quote!(#pat));
+        }
+    }
+    println!(")}}}}");
+}
 
 struct FnVisitor;
 
 impl<'ast> Visit<'ast> for FnVisitor {
     fn visit_item_fn(&mut self, f: &'ast ItemFn) {
-        let name = &f.ident;
-        print!("proptest! {{ #[test] fn test_{}_fuzz (", name);
-        for a in &f.decl.inputs {
-            if let FnArg::Captured(a) = a {
-                let pat = &a.pat;
-                let ty = &a.ty;
-                print!("{}: Any::<{}>(), ", quote!(#pat), quote!(#ty));
-            }
-        }
-        print!(") {{");
-        print!("{} (", name);
-        for a in &f.decl.inputs {
-            if let FnArg::Captured(a) = a {
-                let pat = &a.pat;
-                print!("{}, ", quote!(#pat));
-            }
-        }
-        println!(")}}}}");
+        print_a_test(None, &f.ident, &*f.decl);
         syn::visit::visit_item_fn(self, f);
     }
     fn visit_item_impl(&mut self, f: &'ast ItemImpl) {
         let self_type = &f.self_ty;
+        let self_type = format!("{}", quote!(#self_type));
 
         for item in &f.items {
             if let ImplItem::Method(f) = item {
-                let name = &f.sig.ident;
-                print!("proptest! {{ #[test] fn test_{}_fuzz (", name);
-                print!("self_like_thing: Any::<{}>(), ", quote!(#self_type));
-                for a in &f.sig.decl.inputs {
-                    if let FnArg::Captured(a) = a {
-                        let pat = &a.pat;
-                        let ty = &a.ty;
-                        print!("{}: Any::<{}>(), ", quote!(#pat), quote!(#ty));
-                    }
-                }
-                print!(") {{ self_like_thing.{} (", name);
-                for a in &f.sig.decl.inputs {
-                    if let FnArg::Captured(a) = a {
-                        let pat = &a.pat;
-                        print!("{}, ", quote!(#pat));
-                    }
-                }
-                println!(")}}}}");
+                print_a_test(Some(&self_type), &f.sig.ident, &f.sig.decl);
             }
         }
         syn::visit::visit_item_impl(self, f);
