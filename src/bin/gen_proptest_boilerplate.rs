78d8fc81a17ee237a344bd919b696e00099dc28f
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2019-02-17 19:54:50 +0100

    Abstract away the callback so that the same parser can be used for multiple generators. Paves the way fora fully automated fuzzing harness generator

--- src/bin/gen_proptest_boilerplate.rs
+++ src/bin/gen_proptest_boilerplate.rs
@@ -1,5 +1,7 @@
 use syn;
 use syn::{visit::Visit};
+use syn::{FnArg, FnDecl, Ident, Type};
+use syn::token::{Unsafe, Async};
 use quote::quote;
 use auto_fuzz_test::FnVisitor;
 
@@ -14,5 +16,32 @@ fn main() {
     };
 
     let syntax_tree: syn::File = syn::parse2(code).expect("Failed to parse input. Is it Rust code?");
-    FnVisitor.visit_file(&syntax_tree);
+    FnVisitor{callback: Box::new(print_a_test)}.visit_file(&syntax_tree);
+}
+
+fn print_a_test(this: Option<&Type>, ident: &Ident, decl: &FnDecl, _unsafety: &Option<Unsafe>, _asyncness: &Option<Async>) {
+    print!("proptest! {{ #[test] fn test_{}_fuzz (", ident);
+    if let Some(self_type) = &this {
+        let self_type = format!("{}", quote!(#self_type));
+        print!("self_like_thing: Any::<{}>(), ", self_type);
+    }
+    for a in &decl.inputs {
+        if let FnArg::Captured(a) = a {
+            let pat = &a.pat;
+            let ty = &a.ty;
+            print!("{}: Any::<{}>(), ", quote!(#pat), quote!(#ty));
+        }
+    }
+    print!(") {{");
+    if this.is_some() {
+        print!("self_like_thing.");
+    }
+    print!("{} (", ident);
+    for a in &decl.inputs {
+        if let FnArg::Captured(a) = a {
+            let pat = &a.pat;
+            print!("{}, ", quote!(#pat));
+        }
+    }
+    println!(")}}}}");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,20 @@
-use quote::quote;
-use syn::{visit::Visit, FnArg, FnDecl, Ident, ImplItem, ItemFn, ItemImpl, Type};
+use syn::{visit::Visit, FnDecl, Ident, ImplItem, ItemFn, ItemImpl, Type};
 use syn::token::{Unsafe, Async};
 
-fn print_a_test(this: Option<&Type>, ident: &Ident, decl: &FnDecl, _unsafety: &Option<Unsafe>, _asyncness: &Option<Async>) {
-    print!("proptest! {{ #[test] fn test_{}_fuzz (", ident);
-    if let Some(self_type) = &this {
-        let self_type = format!("{}", quote!(#self_type));
-        print!("self_like_thing: Any::<{}>(), ", self_type);
-    }
-    for a in &decl.inputs {
-        if let FnArg::Captured(a) = a {
-            let pat = &a.pat;
-            let ty = &a.ty;
-            print!("{}: Any::<{}>(), ", quote!(#pat), quote!(#ty));
-        }
-    }
-    print!(") {{");
-    if this.is_some() {
-        print!("self_like_thing.");
-    }
-    print!("{} (", ident);
-    for a in &decl.inputs {
-        if let FnArg::Captured(a) = a {
-            let pat = &a.pat;
-            print!("{}, ", quote!(#pat));
-        }
-    }
-    println!(")}}}}");
+pub struct FnVisitor {
+    pub callback: Box<FnMut(Option<&Type>, &Ident, &FnDecl, &Option<Unsafe>, &Option<Async>) -> ()>
 }
-
-pub struct FnVisitor;
 
 impl<'ast> Visit<'ast> for FnVisitor {
     fn visit_item_fn(&mut self, f: &'ast ItemFn) {
-        print_a_test(None, &f.ident, &*f.decl, &f.unsafety, &f.asyncness);
+        (self.callback)(None, &f.ident, &*f.decl, &f.unsafety, &f.asyncness);
         syn::visit::visit_item_fn(self, f);
     }
     fn visit_item_impl(&mut self, f: &'ast ItemImpl) {
         let self_type = &f.self_ty;
         for item in &f.items {
             if let ImplItem::Method(f) = item {
-                print_a_test(Some(self_type), &f.sig.ident, &f.sig.decl, &f.sig.unsafety, &f.sig.asyncness);
+                (self.callback)(Some(self_type), &f.sig.ident, &f.sig.decl, &f.sig.unsafety, &f.sig.asyncness);
             }
         }
         syn::visit::visit_item_impl(self, f);
