5732084e54602190109b10c6655439751b3415e3
Eugene Lomov <eugene.v.lomov@gmail.com>
2021-02-05 10:49:47 +0300

    Added more tests

--- src/generate.rs
+++ src/generate.rs
@@ -158,6 +158,10 @@ pub fn fuzz_function(signature: &Signature, impl_type: Option<&Type>) -> ItemFn 
         signature.unsafety, None,
         "unsafe functions can not be fuzzed automatically."
     );
+    assert!(
+        !signature.inputs.is_empty(),
+        "It is useless to fuzz function without arguments."
+    );
     //assert!(
     //<Generic type parameter>,
     //"Generics are not currently supported."
@@ -176,15 +180,12 @@ pub fn fuzz_function(signature: &Signature, impl_type: Option<&Type>) -> ItemFn 
             })
             .unwrap();
 
-            // MethodCall inside fuzzing function
             if let Stmt::Semi(Expr::MethodCall(method_call), _) = &mut fuzz_function.block.stmts[0]
             {
+                // MethodCall inside fuzzing function
                 method_call.method = (*signature).ident.clone();
-            }
 
-            // Arguments for internal function call
-            if let Stmt::Semi(Expr::MethodCall(method_call), _) = &mut fuzz_function.block.stmts[0]
-            {
+                // Arguments for internal method call
                 let args = &mut method_call.args;
                 let default_borrowed_field = args.pop().unwrap().into_value();
                 let default_field = args.pop().unwrap().into_value();
@@ -243,7 +244,7 @@ pub fn fuzz_function(signature: &Signature, impl_type: Option<&Type>) -> ItemFn 
                             }
                         }
                         FnArg::Receiver(_) => {
-                            panic!("This macros can not be used for fuzzing methods, use #[create_cargofuzz_impl_harness]")
+                            panic!("Multiple receivers in one function.");
                         }
                     }
                 }
@@ -258,16 +259,14 @@ pub fn fuzz_function(signature: &Signature, impl_type: Option<&Type>) -> ItemFn 
             })
             .unwrap();
 
-            // FnCall inside fuzzing function
             if let Stmt::Semi(Expr::Call(fn_call), _) = &mut fuzz_function.block.stmts[0] {
+                // FnCall inside fuzzing function
                 if let Expr::Path(path) = &mut *fn_call.func {
                     path.path.segments.iter_mut().next().unwrap().ident =
                         (*signature).ident.clone();
                 }
-            }
 
-            // Arguments for internal function call
-            if let Stmt::Semi(Expr::Call(fn_call), _) = &mut fuzz_function.block.stmts[0] {
+                // Arguments for internal function call
                 let args = &mut fn_call.args;
                 let default_borrowed_field = args.pop().unwrap().into_value();
                 let default_field = args.pop().unwrap().into_value();
@@ -384,8 +383,8 @@ pub fn fuzz_harness(signature: &Signature, crate_ident: &Ident, attr: TokenStrea
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::assert_eq;
     use assert_tokens_eq::assert_tokens_eq;
+    use pretty_assertions::assert_eq;
     use syn::ItemImpl;
 
     #[test]
@@ -411,31 +410,7 @@ mod tests {
             }
         })
         .unwrap();
-
         assert_eq!(fuzz_struct(&function.sig, None), fuzz_struct_needed);
-    }
-
-    #[test]
-    fn test_function_no_borrows() {
-        let function: ItemFn = syn::parse2(quote! {
-            pub fn maybe_checked_mul(a: u64, b: u64, crash_on_overflow: bool) -> u64 {
-                if crash_on_overflow {
-                    a.checked_mul(b).expect("Overflow has occurred")
-                } else {
-                    a.overflowing_mul(b).0
-                }
-            }
-        })
-        .unwrap();
-
-        let fuzz_function_needed: ItemFn = syn::parse2(quote! {
-            pub fn __fuzz_maybe_checked_mul(mut input:__fuzz_struct_maybe_checked_mul) {
-                maybe_checked_mul(input.a, input.b, input.crash_on_overflow);
-            }
-        })
-        .unwrap();
-
-        assert_eq!(fuzz_function(&function.sig, None), fuzz_function_needed);
     }
 
     #[test]
@@ -461,62 +436,7 @@ mod tests {
             }
         })
         .unwrap();
-
         assert_eq!(fuzz_struct(&function.sig, None), fuzz_struct_needed);
-    }
-
-    #[test]
-    fn test_function_borrowed() {
-        let function: ItemFn = syn::parse2(quote! {
-            pub fn maybe_checked_mul_borrowed(a: &mut u64, b: u64, crash_on_overflow: bool) {
-                if crash_on_overflow {
-                    *a = a.checked_mul(b).expect("Overflow has occurred");
-                } else {
-                    *a = a.overflowing_mul(b).0;
-                }
-            }
-        })
-        .unwrap();
-
-        let fuzz_function_needed: ItemFn = syn::parse2(
-            quote! {
-                pub fn __fuzz_maybe_checked_mul_borrowed(mut input:__fuzz_struct_maybe_checked_mul_borrowed) {
-                    maybe_checked_mul_borrowed(&mut *input.a, input.b, input.crash_on_overflow);
-                }
-            }
-        ).unwrap();
-
-        assert_eq!(fuzz_function(&function.sig, None), fuzz_function_needed);
-    }
-
-    #[test]
-    fn test_function_harness() {
-        let function: ItemFn = syn::parse2(quote! {
-            pub fn maybe_checked_mul(a: u64, b: u64, crash_on_overflow: bool) -> u64 {
-                if crash_on_overflow {
-                    a.checked_mul(b).expect("Overflow has occurred")
-                } else {
-                    a.overflowing_mul(b).0
-                }
-            }
-        })
-        .unwrap();
-
-        let fuzz_harness_needed = quote! {
-            #![no_main]
-            use libfuzzer_sys::fuzz_target;
-            extern crate test_lib;
-
-            fuzz_target!( |input: test_lib::foo::bar::__fuzz_struct_maybe_checked_mul| {
-                    test_lib::foo::bar::__fuzz_maybe_checked_mul(input);
-                }
-            );
-        };
-
-        let attrs = quote!(foo::bar);
-        let crate_ident = Ident::new("test_lib", Span::call_site());
-        
-        assert_tokens_eq!(fuzz_harness(&function.sig, &crate_ident, attrs), fuzz_harness_needed);
     }
 
     #[test]
@@ -527,6 +447,7 @@ mod tests {
             }
         })
         .unwrap();
+
         let implementation: ItemImpl = syn::parse2(quote! {
             impl TestStruct {
             }
@@ -542,7 +463,6 @@ mod tests {
             }
         })
         .unwrap();
-
         assert_eq!(
             fuzz_struct(&function.sig, Some(&implementation.self_ty)),
             fuzz_struct_needed
@@ -572,10 +492,136 @@ mod tests {
             }
         })
         .unwrap();
-
         assert_eq!(
             fuzz_struct(&function.sig, Some(&implementation.self_ty)),
             fuzz_struct_needed
+        );
+    }
+
+    #[test]
+    fn test_function_unborrowed() {
+        let function: ItemFn = syn::parse2(quote! {
+            pub fn maybe_checked_mul(a: u64, b: u64, crash_on_overflow: bool) -> u64 {
+                if crash_on_overflow {
+                    a.checked_mul(b).expect("Overflow has occurred")
+                } else {
+                    a.overflowing_mul(b).0
+                }
+            }
+        })
+        .unwrap();
+
+        let fuzz_function_needed: ItemFn = syn::parse2(quote! {
+            pub fn __fuzz_maybe_checked_mul(mut input:__fuzz_struct_maybe_checked_mul) {
+                maybe_checked_mul(input.a, input.b, input.crash_on_overflow);
+            }
+        })
+        .unwrap();
+        assert_eq!(fuzz_function(&function.sig, None), fuzz_function_needed);
+    }
+
+    #[test]
+    fn test_function_borrowed() {
+        let function: ItemFn = syn::parse2(quote! {
+            pub fn maybe_checked_mul_borrowed(a: &mut u64, b: u64, crash_on_overflow: bool) {
+                if crash_on_overflow {
+                    *a = a.checked_mul(b).expect("Overflow has occurred");
+                } else {
+                    *a = a.overflowing_mul(b).0;
+                }
+            }
+        })
+        .unwrap();
+
+        let fuzz_function_needed: ItemFn = syn::parse2(
+            quote! {
+                pub fn __fuzz_maybe_checked_mul_borrowed(mut input:__fuzz_struct_maybe_checked_mul_borrowed) {
+                    maybe_checked_mul_borrowed(&mut *input.a, input.b, input.crash_on_overflow);
+                }
+            }
+        ).unwrap();
+        assert_eq!(fuzz_function(&function.sig, None), fuzz_function_needed);
+    }
+
+    #[test]
+    fn test_method_unborrowed() {
+        let function: ItemFn = syn::parse2(quote! {
+            pub fn set_b(self, b: u64) -> u64 {
+                self.b + b
+            }
+        })
+        .unwrap();
+        let implementation: ItemImpl = syn::parse2(quote! {
+            impl TestStruct {
+            }
+        })
+        .unwrap();
+        let fuzz_function_needed: ItemFn = syn::parse2(quote! {
+            pub fn __fuzz_set_b(mut input: __fuzz_struct_set_b) {
+                    (input.slf).set_b(input.b);
+            }
+        })
+        .unwrap();
+        assert_eq!(
+            fuzz_function(&function.sig, Some(&implementation.self_ty)),
+            fuzz_function_needed
+        );
+    }
+
+    #[test]
+    fn test_method_borrowed() {
+        let function: ItemFn = syn::parse2(quote! {
+            pub fn set_b(&mut self, b: u64) {
+                self.b = b;
+            }
+        })
+        .unwrap();
+        let implementation: ItemImpl = syn::parse2(quote! {
+            impl TestStruct {
+            }
+        })
+        .unwrap();
+        let fuzz_function_needed: ItemFn = syn::parse2(quote! {
+            pub fn __fuzz_set_b(mut input: __fuzz_struct_set_b) {
+                    (input.slf).set_b(input.b);
+            }
+        })
+        .unwrap();
+        assert_eq!(
+            fuzz_function(&function.sig, Some(&implementation.self_ty)),
+            fuzz_function_needed
+        );
+    }
+
+    #[test]
+    fn test_harness() {
+        let function: ItemFn = syn::parse2(quote! {
+            pub fn maybe_checked_mul(a: u64, b: u64, crash_on_overflow: bool) -> u64 {
+                if crash_on_overflow {
+                    a.checked_mul(b).expect("Overflow has occurred")
+                } else {
+                    a.overflowing_mul(b).0
+                }
+            }
+        })
+        .unwrap();
+
+        let fuzz_harness_needed = quote! {
+            #![no_main]
+            use libfuzzer_sys::fuzz_target;
+            extern crate test_lib;
+
+            fuzz_target!( |input: test_lib::foo::bar::__fuzz_struct_maybe_checked_mul| {
+                    test_lib::foo::bar::__fuzz_maybe_checked_mul(input);
+                }
+            );
+        };
+
+        let attrs = quote!(foo::bar);
+        let crate_ident = Ident::new("test_lib", Span::call_site());
+        assert_tokens_eq!(
+            fuzz_harness(&function.sig, &crate_ident, attrs),
+            fuzz_harness_needed
         );
     }
 }
